80a87bc1c63e2bf80542de8067ddb5aa3751558b
harshpatidar245 <harshpatidar2400@gmail.com>
2024-09-29 16:58:29 +0530

    Fix bcrypt deployment issue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const RegisterModel = require('./Models/Register');
 const CartModel = require('./Models/CartModel'); // Create this model
